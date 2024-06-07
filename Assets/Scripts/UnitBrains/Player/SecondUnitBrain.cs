c048e8bc64776610b0a8442fba18cecede098680
Spyda-fan <martin-martin-mov@yandex.ru>
2024-06-07 11:20:05 +0300

    ДЗ №7 / Spyda47

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Linq;
+using Model;
 using Model.Runtime.Projectiles;
 using UnityEngine;
+using Utilities;
 
 namespace UnitBrains.Player
 {
@@ -12,6 +15,12 @@ namespace UnitBrains.Player
         private float _temperature = 0f;
         private float _cooldownTime = 0f;
         private bool _overheated;
+
+        private static int unitCounter = 0;
+        private int unitNumber;
+        private const int maxTargets = 3;
+
+        private List<Vector2Int> TargetsOutOfReach = new List<Vector2Int>(); 
         
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
@@ -19,26 +28,74 @@ namespace UnitBrains.Player
             ///////////////////////////////////////
             // Homework 1.3 (1st block, 3rd module)
             ///////////////////////////////////////           
-            var projectile = CreateProjectile(forTarget);
-            AddProjectileToList(projectile, intoList);
+            float currentTemperature = GetTemperature();
+
+            if (currentTemperature >= overheatTemperature)
+                return;
+
+            for (int i = 0; i <= currentTemperature; i++)
+            {
+                var projectile = CreateProjectile(forTarget);
+                AddProjectileToList(projectile, intoList);
+            }
+            IncreaseTemperature();
             ///////////////////////////////////////
         }
-
         public override Vector2Int GetNextStep()
         {
-            return base.GetNextStep();
+            if (TargetsOutOfReach.Count <= 0 || IsTargetInRange(TargetsOutOfReach[0]))
+                return unit.Pos;
+            else 
+                return unit.Pos.CalcNextStepTowards(TargetsOutOfReach[0]);
         }
-
         protected override List<Vector2Int> SelectTargets()
         {
             ///////////////////////////////////////
             // Homework 1.4 (1st block, 4rd module)
             ///////////////////////////////////////
-            List<Vector2Int> result = GetReachableTargets();
-            while (result.Count > 1)
+            List<Vector2Int> result = new List<Vector2Int>();
+            Vector2Int targetPosition;
+
+            TargetsOutOfReach.Clear();
+
+            foreach (Vector2Int target in GetAllTargets())
             {
-                result.RemoveAt(result.Count - 1);
+                TargetsOutOfReach.Add(target);
             }
+
+            if (TargetsOutOfReach.Count == 0)
+            {
+                int enemyBaseId = IsPlayerUnitBrain ? RuntimeModel.BotPlayerId : RuntimeModel.PlayerId;
+                Vector2Int enemyBase = runtimeModel.RoMap.Bases[enemyBaseId];
+                TargetsOutOfReach.Add(enemyBase);
+            }
+            else
+            {
+                SortByDistanceToOwnBase(TargetsOutOfReach);
+
+                int targetIndex = unitNumber % maxTargets;
+
+                if (targetIndex > (TargetsOutOfReach.Count - 1))
+                {
+                    targetPosition = TargetsOutOfReach[0];
+                }
+                else
+                {
+                    if (targetIndex == 0)
+                    {
+                        targetPosition = TargetsOutOfReach[targetIndex];
+                    }
+                    else
+                    {
+                        targetPosition = TargetsOutOfReach[targetIndex - 1];
+                    }
+
+                }
+
+                if (IsTargetInRange(targetPosition))
+                    result.Add(targetPosition);
+            }
+
             return result;
             ///////////////////////////////////////
         }
@@ -57,13 +114,11 @@ namespace UnitBrains.Player
                 }
             }
         }
-
         private int GetTemperature()
         {
             if(_overheated) return (int) OverheatTemperature;
             else return (int)_temperature;
         }
-
         private void IncreaseTemperature()
         {
             _temperature += 1f;
